95c2c22f2f8eb0351e9cbc2c9752e4db23e33f4a
Benno <bp2014@online.de>
2019-09-26 12:53:40 +0200

    added function rating

--- codeBP.R
+++ codeBP.R
@@ -6,6 +6,8 @@
 
 #' list of availabble type in \code{table}
 #'
+#' default relies on `places` present on search path
+#' 
 #' @param table data.frame with location  data
 #' @param ... 
 #'
@@ -15,4 +17,46 @@
 #'
 type.list <- function(table = places, ...){
   return(unique(table$type))
+}
+
+rating <- function(place_id,
+                   type,
+                   table = places,
+                   ...){
+  ratings <- table[table$place_id == place_id & table$type == type, "rating"]
+  if(length(ratings) > 1 && sd(ratings) != 0){
+    warning('ratings from ',min(ratings), ' to ', 'max(ratings)')
+  }
+  return(mean(ratings))
+}
+
+#' plot average occupancy by day/hour
+#'
+#' default relies on `pop` present on search path
+#' 
+#' @param place_id 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+occ.plot <- function(place_id, 
+                     table = pop,
+                     ...){
+  if(place_id %in% pop$place_id){
+    place.data <- subset(pop, 
+                         subset = place_id == place_id)
+    x <- 1:nrow(place.data)
+    plot(place.data$occupancy_index,
+         axes = FALSE
+    )
+    axis(2)
+    axis(1, 
+         at = x,
+         labels = place.data$hhour)
+    mean.x.day <- tapply(x, place.data$day, mean)
+    axis(1, at = mean.x.day,
+         labels = unique(place.data$day))
+  }
 }
