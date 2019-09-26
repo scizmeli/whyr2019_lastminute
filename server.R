d1a565e726062024050029550dbef55333763887
Servet Ahmet Çizmeli <ahmet@pranageo.com>
2019-09-26 15:40:01 +0200

    First version of the Shiny App

---
+++ server.R
@@ -0,0 +1,50 @@
+library(leaflet)
+library(dplyr)
+
+# Define server logic for random distribution app ----
+server <- function(input, output, session) {
+    myTypes = type.list()
+    
+    output$placeTypes <- renderUI({
+         selectInput("plTypes", "What kind of place are you interested in?",
+                     myTypes, myTypes[1])
+    })
+    
+    currentPT <- reactive({
+        pt <- input$mymap_marker_click
+    })
+    points <- reactive({
+        pls <- places %>%
+            filter(type == input$plTypes)
+        # pts <- strsplit(pls$point, split = ",")
+        # lat = sapply(pts, function(x){as.numeric(x[1])})
+        # long = sapply(pts, function(x){as.numeric(x[2])})
+        #data.frame(long=long, lat=lat)
+        data.frame(long=pls$lng, lat=pls$lat, layerId=1:nrow(pls),name=pls$name,
+                   vicinity=pls$vicinity,rating=pls$rating)
+    })
+    
+    
+    output$mymap <- renderLeaflet({
+        myid <- currentPT()$id
+        popupStr = ""
+        if (!is.null(myid))
+            popupStr = paste("Name:", points()$name[myid], "Address:", points()$vicinity[myid],
+               "Type:", points()$type[myid], "Rating:", points()$rating[myid], sep = "<br/>")
+        
+        leaflet() %>%
+            addTiles() %>%
+            addMarkers(data = head(points()[,1:2], 200), layerId = points()[,3], popup = popupStr)
+    })
+    
+    # Generate a summary of the data ----
+    # output$summary <- renderPrint({
+    #     summary(d())
+    # })
+    
+    # Generate an HTML table view of the data ----
+    # output$table <- renderTable({
+    #     d()
+    # })
+    
+}
---
+++ ui.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(leaflet)
+
+# Define UI for random distribution app ----
+ui <- fluidPage(
+    
+    # App title ----
+    titlePanel("Why Warsaw?"),
+    
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        
+        # Sidebar panel for inputs ----
+        sidebarPanel(
+            
+            uiOutput("placeTypes"), 
+            
+            radioButtons("dayOfWeek", "Which day of week to go out?",
+                         c("Monday" = 1,
+                           "Tuesday" = 2,
+                           "Wednesday" = 3,
+                           "Thursday" = 4,
+                           "Friday" = 5,
+                           "Saturday" = 6,
+                           "Sunday" = 7
+                           )),
+            
+            sliderInput("hourOfDay", "What hour of day?", 0, 23, 8),
+            
+            # br() element to introduce extra vertical spacing ----
+            br()
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(
+            
+            # Output: Tabset w/ plot, summary, and table ----
+            tabsetPanel(type = "tabs",
+                        tabPanel("Plot", 
+                                 leafletOutput("mymap")
+                                 )
+                        #,tabPanel("Summary", verbatimTextOutput("summary"))
+                        #,tabPanel("Table", tableOutput("table"))
+            )
+            
+        )
+    )
+)
