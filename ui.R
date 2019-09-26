19067a4120fa7853014735920221d2e725c27cc8
Benno <bp2014@online.de>
2019-09-26 16:06:20 +0200

    removed radiobuttons

--- ui.R
+++ ui.R
@@ -16,28 +16,17 @@ ui <- fluidPage(
             
             uiOutput("placeTypes"), 
             
-<<<<<<< HEAD
+
             # radioButtons("dayOfWeek", "Which day of week to go out?",
-            #              c("Monday" = 1,
-            #                "Tuesday" = 2,
-            #                "Wednesday" = 3,
-            #                "Thursday" = 4,
-            #                "Friday" = 5,
-            #                "Saturday" = 6, 
-            #                "Sunday" = 7
+            #              c("Monday" = "Monday",
+            #                "Tuesday" = "Tuesday",
+            #                "Wednesday" = "Wednesday",
+            #                "Thursday" = "Thursday",
+            #                "Friday" = "Friday",
+            #                "Saturday" = "Saturday",
+            #                "Sunday" = "Sunday"
             #                )),
-=======
-            radioButtons("dayOfWeek", "Which day of week to go out?",
-                         c("Monday" = "Monday",
-                           "Tuesday" = "Tuesday",
-                           "Wednesday" = "Wednesday",
-                           "Thursday" = "Thursday",
-                           "Friday" = "Friday",
-                           "Saturday" = "Saturday",
-                           "Sunday" = "Sunday"
-                           )),
->>>>>>> 21246615b95e565a6bd0b0df66bc676abc9f7417
-            
+
             sliderTextInput("dayOfWeek", "slide for day?", 
                             choices = c("Monday","Tuesday","Wednesday",
                                         "Thursday", "Friday", "Saturday" ,
