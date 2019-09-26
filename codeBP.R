f4c2ad0eb0b7bbeae1af3a66c0923bf3f7a588c3
Benno <bp2014@online.de>
2019-09-26 15:04:42 +0200

    added hour and day as parameters to occ.plot, defaults to now()

--- codeBP.R
+++ codeBP.R
@@ -2,6 +2,7 @@
 # 
 # Benno Pütz
 # 
+require (lubridate)
 
 lu <- function(x){
   length(unique(x))
@@ -87,31 +88,49 @@ ratings.for.type <- function(mytype,
 #'
 #' @examples
 occ.plot <- function(place_id, 
+                     shour = hour(now()),
+                     sday = weekdays(now()),
                      type = NULL, 
                      table = pop,
+                     p.tbl = places,
                      ...){
   if(place_id %in% pop$place_id){
+    days.vector <- c("Monday","Tuesday","Wednesday",
+                     "Thursday", "Friday", "Saturday" ,
+                     "Sunday")
+    if(is.character(sday)){
+      sday <- which(tolower(days.vector) == tolower(sday))
+    }
     if(is.null(type)){
       place.data <- table[table$place_id == place_id,]
     } else {
       place.data <- table[table$place_id == place_id &
                             table$type == type, ]
     }
+    num.day <- as.numeric(ordered(place.data$day,
+                                  levels = days.vector))
     new.day <- c(0,
-                 which(diff(as.numeric(factor(place.data$day))) != 0) - .5,
+                 which(diff(num.day) != 0) - .5,
                  nrow(place.data))
     x <- 1:nrow(place.data)
+    place.name <- paste(p.tbl[p.tbl$place_id == place_id,
+                              'name'][1],
+                        '   ',
+                        p.tbl[p.tbl$place_id == place_id,
+                              'vicinity'][1],
+                        ifelse(is.null(type), '', paste0('[', type, ']')))
     plot(place.data$occupancy_index,
          axes = FALSE,
          xlab = '', 
          ylab = 'occupancy',
          type = 'n',
          xaxs = 'i',
-         yaxs = 'i'
+         yaxs = 'i',
+         main = place.name
     )
     pu <- par('usr')
     rect(new.day[-length(new.day)], pu[3], new.day[-1], pu[4], col = c('#0000ff20','#0000ff10'), border = '#0000ff60')
-    points(place.data$occupancy_index,
+    lines(place.data$occupancy_index,
          xlab = '', 
          ylab = 'occupancy',
     )
@@ -121,6 +140,10 @@ occ.plot <- function(place_id,
          at = x,
          labels = place.data$hour)
     box()
+    if(!(is.null(day) | is.null(hour))){
+      use.idx <- which(num.day == sday & place.data$hour == shour)
+      abline(v=use.idx,, col = 'red')
+    }
     mean.x.day <- tapply(x, place.data$day, mean)
     # omgp <- par('mgp')
     # omgp[3] <- 1
@@ -129,7 +152,7 @@ occ.plot <- function(place_id,
     #      labels = unique(place.data$day))
     # par(opar)
     ud <- unique(place.data$day)
-    text(mean.x.day[ud], -14, ud, xpd = NA)
+    text(mean.x.day[ud], -12, ud, xpd = NA)
   } else{
     warning("no popularity data")
   }
